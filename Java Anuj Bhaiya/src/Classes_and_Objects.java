64c9669e429e1f0900ca7955d11a814d8ef65c90
AgarwalMaddy <madhur1812agarwal@gmail.com>
2023-04-23 10:04:50 +0530

    Constructors
    Learning the concepts of the constructors and their uses.

--- Java Anuj Bhaiya/src/Classes_and_Objects.java
+++ Java Anuj Bhaiya/src/Classes_and_Objects.java
@@ -5,14 +5,19 @@ public class Classes_and_Objects {
         Person p1 = new Person();
         p1.age = 21;
         p1.name = "Madhur Agarwal";
-
-        Person p2 = new Person();
-        p2.age = 31;
-        p2.name = "Anuj Bhaiya";
-
         System.out.println(p1.name + " is " + p1.age + " years old.");
+
+        Person p2 = new Person("Anuj Bhaiya"  , 31);
+        //Default constructor are not used here thus there is no use of below statements to assign values.
+        /* p2.age = 31;
+        p2.name = "Anuj Bhaiya"; */
         System.out.println(p2.name + " is " + p2.age + " years old.");
 
+
+        int totalPersons = Person.count;
+        System.out.println("Total number of persons created using Person class : " + totalPersons);
+
+        System.out.println("Below are all the methods/functions of different persons.");
         p1.eat();
         p2.walk();
         Scanner sc = new Scanner(System.in);
@@ -22,17 +27,31 @@ public class Classes_and_Objects {
     }
 }
 class Person {
-    //Properties of Person
+    //Properties of Person Class
     String name;
     int age;
+    //Creating the properties of the class
+    static int count;
 
+    //Defining the default constructor
+    public Person(){
+        count++;
+        System.out.println("I am a new Person.");
+    }
+
+    //Creating a new constructor with Arguements
+    //A new constructor is created that takes two inputs and assign it to the properties of the class.
+    public Person(String fullName , int realAge){
+        name = fullName;
+        age = realAge;
+    }
+    //These are the methods/functions or properties for the objects(the persons that are created using the class).
     void walk(){
         System.out.println(name + " is walking.");
     }
     void eat(){
         System.out.println(name + " is eating.");
     }
-    
     void walk(int steps){
         System.out.println(name + " has walked " + steps + " steps.");
     }
