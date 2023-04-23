232f38d44fd2339b038c43c43d39325e12968fc8
AgarwalMaddy <madhur1812agarwal@gmail.com>
2023-04-23 10:13:52 +0530

    this()
    Learning and Implementing the use of this() keyword.

--- Java Anuj Bhaiya/src/Classes_and_Objects.java
+++ Java Anuj Bhaiya/src/Classes_and_Objects.java
@@ -5,13 +5,13 @@ public class Classes_and_Objects {
         Person p1 = new Person();
         p1.age = 21;
         p1.name = "Madhur Agarwal";
-        System.out.println(p1.name + " is " + p1.age + " years old.");
+        System.out.println("My name is " + p1.name + " I am  " + p1.age + " years old.");
 
         Person p2 = new Person("Anuj Bhaiya"  , 31);
         //Default constructor are not used here thus there is no use of below statements to assign values.
         /* p2.age = 31;
         p2.name = "Anuj Bhaiya"; */
-        System.out.println(p2.name + " is " + p2.age + " years old.");
+        System.out.println("My name is " + p2.name + " I am  " + p2.age + " years old.");
 
 
         int totalPersons = Person.count;
@@ -39,11 +39,20 @@ class Person {
         System.out.println("I am a new Person.");
     }
 
-    //Creating a new constructor with Arguements
+    //Creating a new constructor with Arguments (Constructor Overloading)
     //A new constructor is created that takes two inputs and assign it to the properties of the class.
-    public Person(String fullName , int realAge){
-        name = fullName;
-        age = realAge;
+    public Person(String name , int age){
+        //this() keyword is used to call other constructors of the same class.
+        this();
+
+       /* name = fullName;
+        age = realAge; */
+
+       /* Now if I had not used different variables in the argument of constructor and used the same as
+          class properties variables then I have to use this() keyword to specify that it is from
+          class that I am assigning it to this variable */
+        this.name = name;
+        this.age = age;
     }
     //These are the methods/functions or properties for the objects(the persons that are created using the class).
     void walk(){
